f3da0a00c05be47845448644d877dc1cd3059966
Rybot <ryan@changepromming.com>
2018-10-20 16:18:05 -0400

    building

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,44 @@
 "use strict";
 
+
+
 module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        jshint: {
-            files: ["*.js", "public/libs/earth/**/*.js"],
-            options: {
-                // ignores: [""],
-                globals: {
-                    Buffer: false,
-                    console: false,
-                    exports: false,
-                    module: false,
-                    process: false,
-                    require: false,
-                    __dirname: false
-                },
-                globalstrict: true
+        // jshint: {
+        //     files: ["*.js", "public/libs/earth/**/*.js"],
+        //     options: {
+        //         // ignores: [""],
+        //         globals: {
+        //             Buffer: false,
+        //             console: false,
+        //             exports: false,
+        //             module: false,
+        //             process: false,
+        //             require: false,
+        //             __dirname: false
+        //         },
+        //         globalstrict: true
+        //     }
+        // },
+        sass: {                              // Task
+            dist: {                            // Target
+                // options: {                       // Target options
+                //     style: 'expanded'
+                // },
+                files: {                         // Dictionary of files
+                    'public/styles/styles.css': 'public/styles/scss/main.scss',       // 'destination': 'source'
+                }
             }
         }
     });
 
     // Load the plugin that provides the "jshint" task.
-    grunt.loadNpmTasks("grunt-contrib-jshint");
+    // grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // Default task(s).
-    grunt.registerTask("default", ["jshint"]);
+    grunt.registerTask("default", ["sass"]);
 
 };
