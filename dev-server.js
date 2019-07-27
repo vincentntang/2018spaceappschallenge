f463971620ece167d6c0127d7b535393dd1c7687
ACpython <10290348+Kagerjay@users.noreply.github.com>
2019-07-27 10:06:28 -0400

    Procfile for Heroku dev instruction deploy env port

--- dev-server.js
+++ dev-server.js
@@ -13,35 +13,36 @@ var util = require("util");
  * Returns true if the response should be compressed.
  */
 function compressionFilter(req, res) {
-    return (/json|text|javascript|font/).test(res.getHeader('Content-Type'));
+  return /json|text|javascript|font/.test(res.getHeader("Content-Type"));
 }
 
 /**
  * Adds headers to a response to enable caching.
  */
 function cacheControl() {
-    return function(req, res, next) {
-        res.setHeader("Cache-Control", "public, max-age=300");
-        return next();
-    };
+  return function(req, res, next) {
+    res.setHeader("Cache-Control", "public, max-age=300");
+    return next();
+  };
 }
 
 function logger() {
-    express.logger.token("date", function() {
-        return new Date().toISOString();
-    });
-    express.logger.token("response-all", function(req, res) {
-        return (res._header ? res._header : "").trim();
-    });
-    express.logger.token("request-all", function(req, res) {
-        return util.inspect(req.headers);
-    });
-    return express.logger(
-        ':date - info: :remote-addr :req[cf-connecting-ip] :req[cf-ipcountry] :method :url HTTP/:http-version ' +
-        '":user-agent" :referrer :req[cf-ray] :req[accept-encoding]\\n:request-all\\n\\n:response-all\\n');
+  express.logger.token("date", function() {
+    return new Date().toISOString();
+  });
+  express.logger.token("response-all", function(req, res) {
+    return (res._header ? res._header : "").trim();
+  });
+  express.logger.token("request-all", function(req, res) {
+    return util.inspect(req.headers);
+  });
+  return express.logger(
+    ":date - info: :remote-addr :req[cf-connecting-ip] :req[cf-ipcountry] :method :url HTTP/:http-version " +
+      '":user-agent" :referrer :req[cf-ray] :req[accept-encoding]\\n:request-all\\n\\n:response-all\\n'
+  );
 }
 
-var port = 8080;
+var port = process.env.PORT || 8080; // for Heroku Deploy
 var express = require("express");
 var app = express();
 var request = require("request");
@@ -53,357 +54,363 @@ app.use(logger());
 app.use(express.static("public"));
 
 class DataNasa {
-    constructor(input) {
-        var data = input["data"][0];
-        this.keywords = data["keywords"];
-        this.title = data["title"];
-        this.description = data["description"];
-        this.center = data["center"];
-        this.media_type = data["media_type"];
-        // this.multimedia_url = input["href"];
-        this.multimedia_url = input["href"];
-    }
+  constructor(input) {
+    var data = input["data"][0];
+    this.keywords = data["keywords"];
+    this.title = data["title"];
+    this.description = data["description"];
+    this.center = data["center"];
+    this.media_type = data["media_type"];
+    // this.multimedia_url = input["href"];
+    this.multimedia_url = input["href"];
+  }
 }
 
-function getNasaStuff(place, req, res){
-    var url = "https://images-api.nasa.gov/search?q=" + place;
+function getNasaStuff(place, req, res) {
+  var url = "https://images-api.nasa.gov/search?q=" + place;
 
-    request(url, function (error, response, body){
-        if (error) {
-            res.json({"Error":"The NASA's servers are not working correctly"});
-        }
-        else {
-            var items = JSON.parse(body)["collection"]["items"];
+  request(url, function(error, response, body) {
+    if (error) {
+      res.json({ Error: "The NASA's servers are not working correctly" });
+    } else {
+      var items = JSON.parse(body)["collection"]["items"];
 
-            var readyToSend = [];
-            var ready = 0;
+      var readyToSend = [];
+      var ready = 0;
 
-            for (var i = 0; i < items.length; i++) {
-                items[i] = new DataNasa(items[i]);
+      for (var i = 0; i < items.length; i++) {
+        items[i] = new DataNasa(items[i]);
 
-                request(items[i].multimedia_url, function (error, response, body){
-                    
-                    //Error while trying to pull image
-                    if(error) {
-                        ready += 1
-                    }
-                    //Request success
-                    else {
-                        if(items[ready] && typeof(items[ready].media_type) != 'undefined' && items[ready].media_type == "image")
-                            items[ready].multimedia_url = JSON.parse(body)[JSON.parse(body).length - 4];
+        request(items[i].multimedia_url, function(error, response, body) {
+          //Error while trying to pull image
+          if (error) {
+            ready += 1;
+          }
+          //Request success
+          else {
+            if (
+              items[ready] &&
+              typeof items[ready].media_type != "undefined" &&
+              items[ready].media_type == "image"
+            )
+              items[ready].multimedia_url = JSON.parse(body)[
+                JSON.parse(body).length - 4
+              ];
 
-                        ready += 1;
-                    }
+            ready += 1;
+          }
 
-                    //Do anyways - When finished send everything
-                    
-                    items = items.filter(x => x.media_type=="image");
-                    res.json({
-                        "Country": place,
-                        "Items": items
-                    });
-                    
-                });
-            }
-        }
-    });
+          //Do anyways - When finished send everything
+
+          items = items.filter(x => x.media_type == "image");
+          res.json({
+            Country: place,
+            Items: items
+          });
+        });
+      }
+    }
+  });
 }
 
-app.get('/api/basic', function(req, res){
-    var latitude = +req.query.lat;
-    var longitude = +req.query.lon;
+app.get("/api/basic", function(req, res) {
+  var latitude = +req.query.lat;
+  var longitude = +req.query.lon;
 
-    //I have to cache the information
-    const options = {
-        url: "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat="+latitude+"&lon="+longitude+"&zoom=0&addressdetails=1&accept-language=en",
-        headers: {
-            'User-Agent': 'Orbis Pictus'
-        }
+  //I have to cache the information
+  const options = {
+    url:
+      "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&zoom=0&addressdetails=1&accept-language=en",
+    headers: {
+      "User-Agent": "Orbis Pictus"
+    }
+  };
+
+  request(options, function(error, response, body) {
+    //Request errors
+    if (error) {
+      res.json({
+        Error: "Something has happend to Nominatim API"
+      });
     }
 
-    request(options, function(error, response, body){
-        //Request errors
-        if(error){
-            res.json({
-                "Error": "Something has happend to Nominatim API"
-            });
-        }
+    //No errors
+    else {
+      var content = JSON.parse(body);
 
-        //No errors
-        else {
-            var content = JSON.parse(body);
-
-            if(content.hasOwnProperty("address")) {
-                var country = alpha2_to_country[content["address"]["country_code"].toUpperCase()];
-                console.log(content);
-                getNasaStuff(country, req, res);
-            }
-            //User hits ocean
-            else {
-                res.json({
-                    "Error": "The user has clicked on water"
-                });
-            }
-        }
-    });
+      if (content.hasOwnProperty("address")) {
+        var country =
+          alpha2_to_country[content["address"]["country_code"].toUpperCase()];
+        console.log(content);
+        getNasaStuff(country, req, res);
+      }
+      //User hits ocean
+      else {
+        res.json({
+          Error: "The user has clicked on water"
+        });
+      }
+    }
+  });
 });
 
-app.get('/api/gravity', function(req, res){
-
-});
-
+app.get("/api/gravity", function(req, res) {});
 
 app.listen(port);
 console.log("Listening on port " + port + "...");
 
 const alpha2_to_country = {
-    "AD": "Andorra",
-    "AE": "United Arab Emirates",
-    "AF": "Afghanistan",
-    "AG": "Antigua and Barbuda",
-    "AI": "Anguilla",
-    "AL": "Albania",
-    "AM": "Armenia",
-    "AO": "Angola",
-    "AQ": "Antarctica",
-    "AR": "Argentina",
-    "AS": "American Samoa",
-    "AT": "Austria",
-    "AU": "Australia",
-    "AW": "Aruba",
-    "AX": "Aland Islands",
-    "AZ": "Azerbaijan",
-    "BA": "Bosnia and Herzegovina",
-    "BB": "Barbados",
-    "BD": "Bangladesh",
-    "BE": "Belgium",
-    "BF": "Burkina Faso",
-    "BG": "Bulgaria",
-    "BH": "Bahrein",
-    "BI": "Burundi",
-    "BJ": "Benin",
-    "BL": "Saint-Barthélemy",
-    "BM": "Bermuda",
-    "BN": "Brunei Darussalam",
-    "BO": "Bolivia",
-    "BQ": "Caribbean Netherlands",
-    "BR": "Brazil",
-    "BS": "Bahamas",
-    "BT": "Bhutan",
-    "BV": "Bouvet Island",
-    "BW": "Botswana",
-    "BY": "Belarus",
-    "BZ": "Belize",
-    "CA": "Canada",
-    "CC": "Cocos (Keeling) Islands",
-    "CD": "Democratic Republic of the Congo",
-    "CF": "Centrafrican Republic",
-    "CG": "Republic of the Congo",
-    "CH": "Switzerland",
-    "CI": "Côte d'Ivoire",
-    "CK": "Cook Islands",
-    "CL": "Chile",
-    "CM": "Cameroon",
-    "CN": "China",
-    "CO": "Colombia",
-    "CR": "Costa Rica",
-    "CU": "Cuba",
-    "CV": "Cabo Verde",
-    "CW": "Curaçao",
-    "CX": "Christmas Island",
-    "CY": "Cyprus",
-    "CZ": "Czech Republic",
-    "DE": "Germany",
-    "DJ": "Djibouti",
-    "DK": "Denmark",
-    "DM": "Dominica",
-    "DO": "Dominican Republic",
-    "DZ": "Algeria",
-    "EC": "Ecuador",
-    "EE": "Estonia",
-    "EG": "Egypt",
-    "EH": "Western Sahara",
-    "ER": "Eritrea",
-    "ES": "Spain",
-    "ET": "Ethiopia",
-    "FI": "Finland",
-    "FJ": "Fiji",
-    "FK": "Falkland Islands",
-    "FM": "Micronesia",
-    "FO": "Faroe Islands",
-    "FR": "France",
-    "GA": "Gabon",
-    "GB": "United Kingdom",
-    "GD": "Grenada",
-    "GE": "Georgia",
-    "GF": "French Guiana",
-    "GG": "Guernsey",
-    "GH": "Ghana",
-    "GI": "Gibraltar",
-    "GL": "Greenland",
-    "GM": "The Gambia",
-    "GN": "Guinea",
-    "GP": "Guadeloupe",
-    "GQ": "Equatorial Guinea",
-    "GR": "Greece",
-    "GS": "South Georgia and the South Sandwich Islands",
-    "GT": "Guatemala",
-    "GU": "Guam",
-    "GW": "Guinea Bissau",
-    "GY": "Guyana",
-    "HK": "Hong Kong",
-    "HM": "Heard Island and McDonald Islands",
-    "HN": "Honduras",
-    "HR": "Croatia",
-    "HT": "Haiti",
-    "HU": "Hungary",
-    "ID": "Indonesia",
-    "IE": "Ireland",
-    "IL": "Israel",
-    "IM": "Isle of Man",
-    "IN": "India",
-    "IO": "British Indian Ocean Territory",
-    "IQ": "Iraq",
-    "IR": "Iran",
-    "IS": "Iceland",
-    "IT": "Italia",
-    "JE": "Jersey",
-    "JM": "Jamaica",
-    "JO": "Jordan",
-    "JP": "Japan",
-    "KE": "Kenya",
-    "KG": "Kyrgyzstan",
-    "KH": "Cambodia",
-    "KI": "Kiribati",
-    "KM": "Comores",
-    "KN": "Saint Kitts and Nevis",
-    "KP": "North Korea",
-    "KR": "South Korea",
-    "KW": "Kuweit",
-    "KY": "Cayman Islands",
-    "KZ": "Kazakhstan",
-    "LA": "Laos",
-    "LB": "Lebanon",
-    "LC": "Saint Lucia",
-    "LI": "Liechtenstein",
-    "LK": "Sri Lanka",
-    "LR": "Liberia",
-    "LS": "Lesotho",
-    "LT": "Lithuania",
-    "LU": "Luxembourg",
-    "LV": "Latvia",
-    "LY": "Libya",
-    "MA": "Morocco",
-    "MC": "Monaco",
-    "MD": "Moldova",
-    "ME": "Montenegro",
-    "MF": "Saint Martin (French part)",
-    "MG": "Madagascar",
-    "MH": "Marshall Islands",
-    "MK": "Macedonia",
-    "ML": "Mali",
-    "MM": "Myanmar",
-    "MN": "Mongolia",
-    "MO": "Macao",
-    "MP": "Northern Mariana Islands",
-    "MQ": "Martinique",
-    "MR": "Mauritania",
-    "MS": "Montserrat",
-    "MT": "Malta",
-    "MU": "Mauritius",
-    "MV": "Maldives",
-    "MW": "Malawi",
-    "MX": "Mexico",
-    "MY": "Malaysia",
-    "MZ": "Mozambique",
-    "NA": "Namibia",
-    "NC": "New Caledonia",
-    "NE": "Niger",
-    "NF": "Norfolk Island",
-    "NG": "Nigeria",
-    "NI": "Nicaragua",
-    "NL": "The Netherlands",
-    "NO": "Norway",
-    "NP": "Nepal",
-    "NR": "Nauru",
-    "NU": "Niue",
-    "NZ": "New Zealand",
-    "OM": "Oman",
-    "PA": "Panama",
-    "PE": "Peru",
-    "PF": "French Polynesia",
-    "PG": "Papua New Guinea",
-    "PH": "Philippines",
-    "PK": "Pakistan",
-    "PL": "Poland",
-    "PM": "Saint Pierre and Miquelon",
-    "PN": "Pitcairn",
-    "PR": "Puerto Rico",
-    "PS": "Palestinian Territory",
-    "PT": "Portugal",
-    "PW": "Palau",
-    "PY": "Paraguay",
-    "QA": "Qatar",
-    "RE": "Reunion",
-    "RO": "Romania",
-    "RS": "Serbia",
-    "RU": "Russia",
-    "RW": "Rwanda",
-    "SA": "Saudi Arabia",
-    "SB": "Solomon Islands",
-    "SC": "Seychelles",
-    "SD": "Sudan",
-    "SE": "Sweden",
-    "SG": "Singapore",
-    "SH": "Saint Helena",
-    "SI": "Slovenia",
-    "SJ": "Svalbard and Jan Mayen",
-    "SK": "Slovakia",
-    "SL": "Sierra Leone",
-    "SM": "San Marino",
-    "SN": "Sénégal",
-    "SO": "Somalia",
-    "SR": "Suriname",
-    "SS": "South Sudan",
-    "ST": "São Tomé and Príncipe",
-    "SV": "El Salvador",
-    "SX": "Saint Martin (Dutch part)",
-    "SY": "Syria",
-    "SZ": "Swaziland",
-    "TC": "Turks and Caicos Islands",
-    "TD": "Chad",
-    "TF": "French Southern and Antarctic Lands",
-    "TG": "Togo",
-    "TH": "Thailand",
-    "TJ": "Tajikistan",
-    "TK": "Tokelau",
-    "TL": "Timor-Leste",
-    "TM": "Turkmenistan",
-    "TN": "Tunisia",
-    "TO": "Tonga",
-    "TR": "Turkey",
-    "TT": "Trinidad and Tobago",
-    "TV": "Tuvalu",
-    "TW": "Taiwan",
-    "TZ": "Tanzania",
-    "UA": "Ukraine",
-    "UG": "Uganda",
-    "UM": "United States Minor Outlying Islands",
-    "US": "United States of America",
-    "UY": "Uruguay",
-    "UZ": "Uzbekistan",
-    "VA": "City of the Vatican",
-    "VC": "Saint Vincent and the Grenadines",
-    "VE": "Venezuela",
-    "VG": "British Virgin Islands",
-    "VI": "United States Virgin Islands",
-    "VN": "Vietnam",
-    "VU": "Vanuatu",
-    "WF": "Wallis and Futuna",
-    "WS": "Samoa",
-    "YE": "Yemen",
-    "YT": "Mayotte",
-    "ZA": "South Africa",
-    "ZM": "Zambia",
-    "ZW": "Zimbabwe"
-  };
+  AD: "Andorra",
+  AE: "United Arab Emirates",
+  AF: "Afghanistan",
+  AG: "Antigua and Barbuda",
+  AI: "Anguilla",
+  AL: "Albania",
+  AM: "Armenia",
+  AO: "Angola",
+  AQ: "Antarctica",
+  AR: "Argentina",
+  AS: "American Samoa",
+  AT: "Austria",
+  AU: "Australia",
+  AW: "Aruba",
+  AX: "Aland Islands",
+  AZ: "Azerbaijan",
+  BA: "Bosnia and Herzegovina",
+  BB: "Barbados",
+  BD: "Bangladesh",
+  BE: "Belgium",
+  BF: "Burkina Faso",
+  BG: "Bulgaria",
+  BH: "Bahrein",
+  BI: "Burundi",
+  BJ: "Benin",
+  BL: "Saint-Barthélemy",
+  BM: "Bermuda",
+  BN: "Brunei Darussalam",
+  BO: "Bolivia",
+  BQ: "Caribbean Netherlands",
+  BR: "Brazil",
+  BS: "Bahamas",
+  BT: "Bhutan",
+  BV: "Bouvet Island",
+  BW: "Botswana",
+  BY: "Belarus",
+  BZ: "Belize",
+  CA: "Canada",
+  CC: "Cocos (Keeling) Islands",
+  CD: "Democratic Republic of the Congo",
+  CF: "Centrafrican Republic",
+  CG: "Republic of the Congo",
+  CH: "Switzerland",
+  CI: "Côte d'Ivoire",
+  CK: "Cook Islands",
+  CL: "Chile",
+  CM: "Cameroon",
+  CN: "China",
+  CO: "Colombia",
+  CR: "Costa Rica",
+  CU: "Cuba",
+  CV: "Cabo Verde",
+  CW: "Curaçao",
+  CX: "Christmas Island",
+  CY: "Cyprus",
+  CZ: "Czech Republic",
+  DE: "Germany",
+  DJ: "Djibouti",
+  DK: "Denmark",
+  DM: "Dominica",
+  DO: "Dominican Republic",
+  DZ: "Algeria",
+  EC: "Ecuador",
+  EE: "Estonia",
+  EG: "Egypt",
+  EH: "Western Sahara",
+  ER: "Eritrea",
+  ES: "Spain",
+  ET: "Ethiopia",
+  FI: "Finland",
+  FJ: "Fiji",
+  FK: "Falkland Islands",
+  FM: "Micronesia",
+  FO: "Faroe Islands",
+  FR: "France",
+  GA: "Gabon",
+  GB: "United Kingdom",
+  GD: "Grenada",
+  GE: "Georgia",
+  GF: "French Guiana",
+  GG: "Guernsey",
+  GH: "Ghana",
+  GI: "Gibraltar",
+  GL: "Greenland",
+  GM: "The Gambia",
+  GN: "Guinea",
+  GP: "Guadeloupe",
+  GQ: "Equatorial Guinea",
+  GR: "Greece",
+  GS: "South Georgia and the South Sandwich Islands",
+  GT: "Guatemala",
+  GU: "Guam",
+  GW: "Guinea Bissau",
+  GY: "Guyana",
+  HK: "Hong Kong",
+  HM: "Heard Island and McDonald Islands",
+  HN: "Honduras",
+  HR: "Croatia",
+  HT: "Haiti",
+  HU: "Hungary",
+  ID: "Indonesia",
+  IE: "Ireland",
+  IL: "Israel",
+  IM: "Isle of Man",
+  IN: "India",
+  IO: "British Indian Ocean Territory",
+  IQ: "Iraq",
+  IR: "Iran",
+  IS: "Iceland",
+  IT: "Italia",
+  JE: "Jersey",
+  JM: "Jamaica",
+  JO: "Jordan",
+  JP: "Japan",
+  KE: "Kenya",
+  KG: "Kyrgyzstan",
+  KH: "Cambodia",
+  KI: "Kiribati",
+  KM: "Comores",
+  KN: "Saint Kitts and Nevis",
+  KP: "North Korea",
+  KR: "South Korea",
+  KW: "Kuweit",
+  KY: "Cayman Islands",
+  KZ: "Kazakhstan",
+  LA: "Laos",
+  LB: "Lebanon",
+  LC: "Saint Lucia",
+  LI: "Liechtenstein",
+  LK: "Sri Lanka",
+  LR: "Liberia",
+  LS: "Lesotho",
+  LT: "Lithuania",
+  LU: "Luxembourg",
+  LV: "Latvia",
+  LY: "Libya",
+  MA: "Morocco",
+  MC: "Monaco",
+  MD: "Moldova",
+  ME: "Montenegro",
+  MF: "Saint Martin (French part)",
+  MG: "Madagascar",
+  MH: "Marshall Islands",
+  MK: "Macedonia",
+  ML: "Mali",
+  MM: "Myanmar",
+  MN: "Mongolia",
+  MO: "Macao",
+  MP: "Northern Mariana Islands",
+  MQ: "Martinique",
+  MR: "Mauritania",
+  MS: "Montserrat",
+  MT: "Malta",
+  MU: "Mauritius",
+  MV: "Maldives",
+  MW: "Malawi",
+  MX: "Mexico",
+  MY: "Malaysia",
+  MZ: "Mozambique",
+  NA: "Namibia",
+  NC: "New Caledonia",
+  NE: "Niger",
+  NF: "Norfolk Island",
+  NG: "Nigeria",
+  NI: "Nicaragua",
+  NL: "The Netherlands",
+  NO: "Norway",
+  NP: "Nepal",
+  NR: "Nauru",
+  NU: "Niue",
+  NZ: "New Zealand",
+  OM: "Oman",
+  PA: "Panama",
+  PE: "Peru",
+  PF: "French Polynesia",
+  PG: "Papua New Guinea",
+  PH: "Philippines",
+  PK: "Pakistan",
+  PL: "Poland",
+  PM: "Saint Pierre and Miquelon",
+  PN: "Pitcairn",
+  PR: "Puerto Rico",
+  PS: "Palestinian Territory",
+  PT: "Portugal",
+  PW: "Palau",
+  PY: "Paraguay",
+  QA: "Qatar",
+  RE: "Reunion",
+  RO: "Romania",
+  RS: "Serbia",
+  RU: "Russia",
+  RW: "Rwanda",
+  SA: "Saudi Arabia",
+  SB: "Solomon Islands",
+  SC: "Seychelles",
+  SD: "Sudan",
+  SE: "Sweden",
+  SG: "Singapore",
+  SH: "Saint Helena",
+  SI: "Slovenia",
+  SJ: "Svalbard and Jan Mayen",
+  SK: "Slovakia",
+  SL: "Sierra Leone",
+  SM: "San Marino",
+  SN: "Sénégal",
+  SO: "Somalia",
+  SR: "Suriname",
+  SS: "South Sudan",
+  ST: "São Tomé and Príncipe",
+  SV: "El Salvador",
+  SX: "Saint Martin (Dutch part)",
+  SY: "Syria",
+  SZ: "Swaziland",
+  TC: "Turks and Caicos Islands",
+  TD: "Chad",
+  TF: "French Southern and Antarctic Lands",
+  TG: "Togo",
+  TH: "Thailand",
+  TJ: "Tajikistan",
+  TK: "Tokelau",
+  TL: "Timor-Leste",
+  TM: "Turkmenistan",
+  TN: "Tunisia",
+  TO: "Tonga",
+  TR: "Turkey",
+  TT: "Trinidad and Tobago",
+  TV: "Tuvalu",
+  TW: "Taiwan",
+  TZ: "Tanzania",
+  UA: "Ukraine",
+  UG: "Uganda",
+  UM: "United States Minor Outlying Islands",
+  US: "United States of America",
+  UY: "Uruguay",
+  UZ: "Uzbekistan",
+  VA: "City of the Vatican",
+  VC: "Saint Vincent and the Grenadines",
+  VE: "Venezuela",
+  VG: "British Virgin Islands",
+  VI: "United States Virgin Islands",
+  VN: "Vietnam",
+  VU: "Vanuatu",
+  WF: "Wallis and Futuna",
+  WS: "Samoa",
+  YE: "Yemen",
+  YT: "Mayotte",
+  ZA: "South Africa",
+  ZM: "Zambia",
+  ZW: "Zimbabwe"
+};
